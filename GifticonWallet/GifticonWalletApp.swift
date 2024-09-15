af2bb26a6972f73505e8c20367238cd1a844a0af
김정완 <krwd1117@gmail.com>
2024-09-15 22:20:20 +0900

    프로젝트 생성

---
+++ GifticonWallet/GifticonWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifticonWalletApp.swift
+//  GifticonWallet
+//
+//  Created by Jeongwan Kim on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GifticonWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
